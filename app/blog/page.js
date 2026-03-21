2d6083477bb7302feb40a765760e1a4a0af20d81
Abhay <abhay@cleardigital.com>
2026-03-21 16:36:02 +0530

    set date feature

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { getAllPosts } from '../../lib/posts'
 import BannerSecond from '../components/BannerSecond'
 import Featuredimage from '../components/Featuredimage'
+import Date from '../components/Date'
 
 export const metadata = {
   title: 'Blog - Cool Nomad',
@@ -18,35 +19,38 @@ const page = async () => {
       <main>
         <section className='post-list mt-4'>
           <div className='container'>
-          <ul>
-            {
-              allPosts?.nodes?.map((post) => (
-                <li key={post.slug} className='grid grid-cols-5 gap-4 mb-4'>
-                  <div className='col-span-2 '>
-                    <Featuredimage post={post}/>
-                  </div>
-                  <div className='col-span-3'>
-                    <h2 className='py-4'>
-                      <Link href={`/blog/${post.slug}`} className='text-blue-400 text-2xl hover:text-blue-600'>
-                        {post.title}
-                      </Link>
-                    </h2>
-                    <div className='text-lg' dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                      <div className='py-4'>
-                     Posted under {
-                       post.categories?.nodes?.map((cat) => (
-                        <Link className='text-blue-400 hover:text-blue-500' key={cat.slug} href={`/category/${cat.slug}`}>
-                          {cat.name}
+            <ul>
+              {
+                allPosts?.nodes?.map((post) => (
+                  <li key={post.slug} className='grid grid-cols-5 gap-4 mb-4'>
+                    <div className='col-span-2 '>
+                      <Featuredimage post={post} />
+                    </div>
+                    <div className='col-span-3'>
+                      <h2 className='py-4'>
+                        <Link href={`/blog/${post.slug}`} className='text-blue-400 text-2xl hover:text-blue-600'>
+                          {post.title}
                         </Link>
-                       ))
-                     }
+                      </h2>
+                      <div className='py-4'>
+                        Published on: <Date dateString={post.date} />
                       </div>
-                  </div>
-    
-                </li>
-              ))
-            }
-          </ul>
+                      <div className='text-lg' dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+                      <div className='py-4'>
+                        Posted under {
+                          post.categories?.nodes?.map((cat) => (
+                            <Link className='text-blue-400 hover:text-blue-500' key={cat.slug} href={`/category/${cat.slug}`}>
+                              {cat.name}
+                            </Link>
+                          ))
+                        }
+                      </div>
+                    </div>
+
+                  </li>
+                ))
+              }
+            </ul>
           </div>
         </section>
       </main>
